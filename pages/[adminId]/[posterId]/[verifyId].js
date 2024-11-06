84f8cc047e1774047e88c38fea6e0f588bb90fff
Zayn Yusuf <yusue786@gmail.com>
2024-11-06 22:08:44 +0600

    adding new site route path

--- pages/[adminId]/[posterId]/[verifyId].js
+++ pages/[adminId]/[posterId]/[verifyId].js
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { API_URL, site } from "../../../../config/index";
-import useMockLogin from "../../../../hooks/useMockLogin";
-import Header from "../../../../components/Header";
-import Login from "../../../../components/Login";
-import Footer from "../../../../components/Footer";
-import Security from "../../../../components/Security";
+import { API_URL, site } from "../../../config/index";
+import useMockLogin from "../../../hooks/useMockLogin";
+import Header from "../../../components/Header";
+import Login from "../../../components/Login";
+import Footer from "../../../components/Footer";
+import Security from "../../../components/Security";
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
